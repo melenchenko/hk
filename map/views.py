9ec0ac4cf649fd0de81142521e85fd39c32ef581
prostakov_al <prostakov.a@ittest-team.ru>
2019-06-19 17:42:27 +0300

    Добавлена авторизация

--- map/views.py
+++ map/views.py
@@ -3,8 +3,11 @@ import random
 import datetime
 from plotly.offline import plot
 import map.utils as ut
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 
 
+@login_required(login_url=reverse_lazy('login'))
 def city_map(request):
     city = plot(ut.town(), output_type='div', include_plotlyjs=True)
     #assert False
--- welcome/settings.py
+++ welcome/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'graphs',
     'map',
     'tables',
+    'person',
 ]
 
 MIDDLEWARE = [
