4d8cd4b84e2dd4bfa67800096f03e1f1d3ffed2f
Roman Melenchenko <melenchenko@yandex.ru>
2019-06-21 08:54:00 +0300

    static catalogs

--- welcome/settings.py
+++ welcome/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'uploads/'),
+    os.path.join(BASE_DIR, 'fits.dumps/'),
 ]
 DATABASES = {
     'default': {
