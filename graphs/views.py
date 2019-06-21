53821d3d6377c109a7d2ee2205866eed37fecd3c
prostakov_al <prostakov.a@ittest-team.ru>
2019-06-21 18:16:57 +0300

    Добавлен график и функция для апроксимации

--- graphs/views.py
+++ graphs/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from plotly.offline import plot
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from graphs.utils import big_graph
+import csv
+import scipy as sp
 
 
+@login_required(login_url=reverse_lazy('login'))
 def graph(request):
-    return 'Test'
+    b_graph = plot(big_graph(), output_type='div', include_plotlyjs=True)
+    #assert False
+    return render(request, 'graph.html', context=dict(big_graph=b_graph))
+
