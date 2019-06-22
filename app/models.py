61d2d87b284aaba628b72536693a86ea46a1a06a
prostakov_al <prostakov.a@ittest-team.ru>
2019-06-22 13:45:59 +0300

    Работаю над заполнением

--- app/models.py
+++ app/models.py
@@ -16,10 +16,16 @@ class Load(models.Model):
 class Region(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class City(models.Model):
     name = models.CharField(max_length=100)
     region = models.ForeignKey(Region, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 
 class Settings(models.Model):
